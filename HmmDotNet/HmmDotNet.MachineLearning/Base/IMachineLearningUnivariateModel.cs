f14ad49a6409b885f869c45abf46c9e397c94f67
Lev Brailovskiy <lev4ik@gmail.com>
2013-06-14 16:53:19 +0300

    Removed unnecessary property

--- HmmDotNet/HmmDotNet.MachineLearning/Base/IMachineLearningUnivariateModel.cs
+++ HmmDotNet/HmmDotNet.MachineLearning/Base/IMachineLearningUnivariateModel.cs
@@ -3,11 +3,6 @@
     public interface IMachineLearningUnivariateModel
     {
         /// <summary>
-        ///     Likelihood of given model
-        /// </summary>
-        double Likelihood { get; }
-
-        /// <summary>
         ///     Performs training procedure for the model
         /// </summary>
         /// <param name="observations"></param>
